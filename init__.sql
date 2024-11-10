30fd42d1fbf96ec106b68c3c3fa05afd5e9c00e7
yuity21 <mouktarryan21@gmail.com>
2024-11-10 22:50:58 +0100

    Ajout d'un event sql pour la maj automatique des états des projets si end_date dépassé

--- init__.sql
+++ init__.sql
@@ -28,3 +28,15 @@ CREATE TABLE project_members (
     FOREIGN KEY (project_id) REFERENCES projects(id),
     FOREIGN KEY (username) REFERENCES users(username)
 );
+
+-- Active l'event scheduler
+SET GLOBAL event_scheduler = ON;
+
+-- Création d'un event qui permet que si un projet atteint sa deadline alors il sera automatiquement terminé
+CREATE EVENT IF NOT EXISTS update_project_state
+ON SCHEDULE EVERY 1 DAY -- Met à jour tous les jours
+DO
+  UPDATE projects
+  SET state = 'terminé'
+  WHERE state = 'en cours' AND end_date <= CURDATE();
+
